535397d7dbd619687b833d02fc174955af709ef9
Taleh Ibrahimli <taleh.ibrahimli@aurea.com>
2019-06-29 18:30:47 +0400

    migrate to gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    java
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    // Use jcenter for resolving your dependencies.
+    // You can declare any Maven/Ivy/file repository here.
+    jcenter()
+}
+
+dependencies {
+
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.9.9")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.9.9")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.9")
+
+    // Use JUnit test framework
+    testImplementation("junit:junit:4.12")
+}
